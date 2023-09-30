3abe23aeea7b24db870d2f40d74b40fa878a6d22
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-09-29 20:58:24 -0400

    Scaffold application

---
+++ src/main/java/tools/sctrade/companion/CompanionApplication.java
@@ -0,0 +1,58 @@
+package tools.sctrade.companion;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class CompanionApplication extends JFrame {
+  private static final long serialVersionUID = 1L;
+
+  public CompanionApplication() {
+    initUI();
+  }
+
+  private void initUI() {
+    var quitButton = new JButton("Quit");
+
+    quitButton.addActionListener((ActionEvent event) -> {
+      System.exit(0);
+    });
+
+    createLayout(quitButton);
+
+    setTitle("Quit button");
+    setSize(300, 200);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+  private void createLayout(JComponent... arg) {
+    var pane = getContentPane();
+    var gl = new GroupLayout(pane);
+    pane.setLayout(gl);
+
+    gl.setAutoCreateContainerGaps(true);
+
+    gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(arg[0]));
+
+    gl.setVerticalGroup(gl.createSequentialGroup().addComponent(arg[0]));
+  }
+
+  public static void main(String[] args) {
+    var ctx = new SpringApplicationBuilder(CompanionApplication.class).headless(false)
+        .web(WebApplicationType.NONE).run(args);
+
+    EventQueue.invokeLater(() -> {
+
+      var ex = ctx.getBean(CompanionApplication.class);
+      ex.setVisible(true);
+    });
+  }
+}
