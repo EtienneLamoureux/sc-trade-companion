725e7e66257907b5b8e76973a5ab86cba395e15b
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-10-04 20:44:49 -0400

    Hook up screen capture code

--- src/main/java/tools/sctrade/companion/KeyListener.java
+++ src/main/java/tools/sctrade/companion/KeyListener.java
@@ -1,31 +1,31 @@
 package tools.sctrade.companion;
 
-import com.github.kwhat.jnativehook.GlobalScreen;
-import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class KeyListener implements NativeKeyListener {
   @Override
   public void nativeKeyPressed(NativeKeyEvent e) {
     System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 
-    if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+    if (e.getKeyCode() == NativeKeyEvent.VC_F3) {
       try {
-        GlobalScreen.unregisterNativeHook();
-      } catch (NativeHookException nativeHookException) {
-        nativeHookException.printStackTrace();
+        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage capture = new Robot().createScreenCapture(screenRect);
+
+        File imageFile = new File("single-screen.bmp");
+        ImageIO.write(capture, "bmp", imageFile);
+      } catch (IOException | AWTException e1) {
+        e1.printStackTrace();
       }
     }
-  }
-
-  @Override
-  public void nativeKeyReleased(NativeKeyEvent e) {
-    System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-  }
-
-  @Override
-  public void nativeKeyTyped(NativeKeyEvent e) {
-    System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
   }
 }
