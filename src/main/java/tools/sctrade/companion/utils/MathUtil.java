7badc13bfbed208bb18ebe2593dc5cf90dad0cf4
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-10-23 14:15:15 -0400

    Deal with outliers when splitting column paragraphs

---
+++ src/main/java/tools/sctrade/companion/utils/MathUtil.java
@@ -0,0 +1,19 @@
+package tools.sctrade.companion.utils;
+
+import java.util.List;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class MathUtil {
+  public static List<Double> calculateOuliers(List<Double> values) {
+    DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+    values.stream().forEach(n -> descriptiveStatistics.addValue(n));
+
+    double q1 = descriptiveStatistics.getPercentile(25);
+    double q3 = descriptiveStatistics.getPercentile(75);
+    double interQuartileRange = q3 - q1;
+    double lowerFence = q1 - (1.5 * interQuartileRange);
+    double upperFence = q3 + (1.5 * interQuartileRange);
+
+    return values.stream().filter(n -> n < lowerFence || n > upperFence).toList();
+  }
+}
---
+++ src/test/java/tools/sctrade/companion/utils/MathUtilTest.java
@@ -0,0 +1,26 @@
+package tools.sctrade.companion.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class MathUtilTest {
+  @Test
+  void givenDistributionWithLongTailWhenCalculatingOuliersThenReturnTail() {
+    List<Double> distribution = Arrays.asList(12.0, 13.0, 14.0, 14.0, 14.0, 15.0, 15.0, 16.0, 16.0,
+        57.0, 57.0, 58.0, 58.0, 58.0, 58.0, 58.0, 206.0);
+    var outliers = MathUtil.calculateOuliers(distribution);
+
+    assertEquals(Arrays.asList(206.0), outliers);
+  }
+
+  @Test
+  void givenSmallDistributionWhenCalculatingOuliersThenReturnEmptyList() {
+    List<Double> distribution = Arrays.asList(1.0);
+    var outliers = MathUtil.calculateOuliers(distribution);
+
+    assertTrue(outliers.isEmpty());
+  }
+}
