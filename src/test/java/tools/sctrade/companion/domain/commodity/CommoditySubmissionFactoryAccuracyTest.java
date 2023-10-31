85eb5f298a399f7859bc1ab06056bbf02082b5ea
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-10-31 09:34:59 -0400

    Manually test thresholding

--- src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryAccuracyTest.java
+++ src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryAccuracyTest.java
@@ -7,9 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tools.sctrade.companion.domain.image.ImageManipulation;
-import tools.sctrade.companion.domain.image.manipulations.AdjustBrightnessAndContrast;
-import tools.sctrade.companion.domain.image.manipulations.ConvertToGreyscale;
-import tools.sctrade.companion.domain.image.manipulations.InvertColors;
 import tools.sctrade.companion.domain.image.manipulations.UpscaleTo4k;
 import tools.sctrade.companion.utils.ImageUtil;
 
@@ -21,9 +18,9 @@ public class CommoditySubmissionFactoryAccuracyTest {
   @BeforeEach
   void setUp() {
     List<ImageManipulation> preprocessingManipulations = new ArrayList<>();
-    preprocessingManipulations.add(new ConvertToGreyscale());
-    preprocessingManipulations.add(new InvertColors());
-    preprocessingManipulations.add(new AdjustBrightnessAndContrast(10.0f, 0.0f));
+    // preprocessingManipulations.add(new ConvertToGreyscale());
+    // preprocessingManipulations.add(new InvertColors());
+    // preprocessingManipulations.add(new AdjustBrightnessAndContrast(10.0f, 0.0f));
     // preprocessingManipulations.add(new WriteToDisk(PREPROCESSED_FOLDER_PATH));
 
     factory = new CommoditySubmissionFactory(null,
@@ -33,8 +30,8 @@ public class CommoditySubmissionFactoryAccuracyTest {
   @Test
   void givenCorrectSreenshotThenReadTextAccurately() throws IOException {
     var manipulation = new UpscaleTo4k();
-    BufferedImage screenshot = ImageUtil
-        .getFromResourcePath("/images/kiosks/commodity/ScreenShot-2023-10-27_15-28-07-12F.jpg");
+    BufferedImage screenshot =
+        ImageUtil.getFromResourcePath("/images/kiosks/commodity/2023-10-31_9-27-0-224873600.jpg");
     screenshot = manipulation.manipulate(screenshot);
 
     factory.build(screenshot);
