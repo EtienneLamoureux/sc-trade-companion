7fe1220dd1263ee52081488c994e23c221e9e677
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-02 21:01:40 -0400

    Remove non-words from Tesseract results

--- src/main/java/tools/sctrade/companion/domain/commodity/CommodityLocationTesseractOcr.java
+++ src/main/java/tools/sctrade/companion/domain/commodity/CommodityLocationTesseractOcr.java
@@ -1,10 +1,8 @@
 package tools.sctrade.companion.domain.commodity;
 
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.stream.Collectors;
-import net.sourceforge.tess4j.Word;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tools.sctrade.companion.domain.image.ImageManipulation;
@@ -23,6 +21,8 @@ public class CommodityLocationTesseractOcr extends TesseractOcr {
   public OcrResult process(BufferedImage image) {
     image = cropRightHalf(image);
     var words = tesseract.getWords(image, 0);
+    words = removeNonWords(words);
+    words = removeSingleCharacterWords(words);
     logger.trace(
         words.stream().map(n -> n.getText()).collect(Collectors.joining(System.lineSeparator())));
 
@@ -32,10 +32,5 @@ public class CommodityLocationTesseractOcr extends TesseractOcr {
 
   private BufferedImage cropRightHalf(BufferedImage image) {
     return image.getSubimage(0, 0, (image.getWidth() / 2), image.getHeight());
-  }
-
-  private List<Word> onlyKeepWordsInLeftHalfOfImage(BufferedImage image, List<Word> words) {
-    Rectangle leftHalf = new Rectangle(0, 0, (image.getWidth() / 2), image.getHeight());
-    return words.stream().filter(n -> leftHalf.contains(n.getBoundingBox())).toList();
   }
 }
--- src/main/java/tools/sctrade/companion/domain/ocr/TesseractOcr.java
+++ src/main/java/tools/sctrade/companion/domain/ocr/TesseractOcr.java
@@ -3,6 +3,7 @@ package tools.sctrade.companion.domain.ocr;
 import java.util.Collections;
 import java.util.List;
 import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.Word;
 import tools.sctrade.companion.domain.image.ImageManipulation;
 
 public abstract class TesseractOcr extends Ocr {
@@ -22,4 +23,11 @@ public abstract class TesseractOcr extends Ocr {
     tesseract.setOcrEngineMode(3);
   }
 
+  protected List<Word> removeSingleCharacterWords(List<Word> words) {
+    return words.stream().filter(n -> n.getText().strip().length() > 1).toList();
+  }
+
+  protected List<Word> removeNonWords(List<Word> words) {
+    return words.stream().filter(n -> !n.getText().matches("[^a-zA-Z0-9]+")).toList();
+  }
 }
