307f0bbe0b02dc1297d053c187edc2b6e5ab9cc5
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-06 16:00:49 -0500

    Implement CommoditySubmission.merge

--- src/main/java/tools/sctrade/companion/domain/commodity/CommoditySubmission.java
+++ src/main/java/tools/sctrade/companion/domain/commodity/CommoditySubmission.java
@@ -12,8 +12,31 @@ public class CommoditySubmission {
     this.listings = listings;
   }
 
-  synchronized void merge(CommoditySubmission submission) {
-    // TODO Auto-generated method stub
+  User getUser() {
+    return user;
+  }
 
+  Collection<CommodityListing> getListings() {
+    return listings;
+  }
+
+  synchronized void merge(CommoditySubmission submission) {
+    CommodityListing newListing = submission.getListings().iterator().next();
+
+    /*
+     * Kiosk location can only be correctly acquired from the "Sell" tab, since on the "Buy" tab,
+     * the inventory listed would be the ship.
+     */
+    if (newListing.transaction().equals(TransactionType.BUYS.toString())) {
+      listings = listings.parallelStream().map(listing -> {
+        if (listing.transaction().equals(TransactionType.SELLS.toString())) {
+          return listing;
+        }
+
+        return listing.withLocation(newListing.location());
+      }).toList();
+    }
+
+    listings.addAll(submission.getListings());
   }
 }
