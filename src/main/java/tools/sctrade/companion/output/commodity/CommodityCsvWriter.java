a9cbcf3f017bf52fb4bafb153f840104b19fa4d9
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-06 16:28:11 -0500

    Add logs

--- src/main/java/tools/sctrade/companion/output/commodity/CommodityCsvWriter.java
+++ src/main/java/tools/sctrade/companion/output/commodity/CommodityCsvWriter.java
@@ -1,11 +1,20 @@
 package tools.sctrade.companion.output.commodity;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tools.sctrade.companion.domain.commodity.CommoditySubmission;
 import tools.sctrade.companion.utils.AsynchronousProcessor;
 
 public class CommodityCsvWriter extends AsynchronousProcessor<CommoditySubmission> {
+  private final Logger logger = LoggerFactory.getLogger(CommodityCsvWriter.class);
+
+  private Path outputPath;
+
   public CommodityCsvWriter() {
-    // TODO Auto-generated constructor stub
+    outputPath = Paths.get("./my-data").normalize().toAbsolutePath();
+    logger.info("CSV output path: {}", outputPath);
   }
 
   @Override
