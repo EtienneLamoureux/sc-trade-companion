71a4568ddbb27ec448090fde7aefdbf873baba62
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-06 16:00:23 -0500

    Setup CSV util

--- src/main/java/tools/sctrade/companion/output/commodity/CommodityCsvWriter.java
+++ src/main/java/tools/sctrade/companion/output/commodity/CommodityCsvWriter.java
@@ -4,12 +4,13 @@ import tools.sctrade.companion.domain.commodity.CommoditySubmission;
 import tools.sctrade.companion.utils.AsynchronousProcessor;
 
 public class CommodityCsvWriter extends AsynchronousProcessor<CommoditySubmission> {
+  public CommodityCsvWriter() {
+    // TODO Auto-generated constructor stub
+  }
 
   @Override
   public void process(CommoditySubmission submission) {
     // TODO Auto-generated method stub
 
   }
-
-
 }
