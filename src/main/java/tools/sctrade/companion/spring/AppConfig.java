fd4c955586a9b97d8efc581de3df26c11398b83a
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-13 20:45:32 -0500

    Fix multi-threaded OCR usage

--- src/main/java/tools/sctrade/companion/spring/AppConfig.java
+++ src/main/java/tools/sctrade/companion/spring/AppConfig.java
@@ -8,17 +8,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tools.sctrade.companion.domain.commodity.CommodityListingsTesseractOcr;
-import tools.sctrade.companion.domain.commodity.CommodityLocationTesseractOcr;
+import org.springframework.web.reactive.function.client.WebClient;
 import tools.sctrade.companion.domain.commodity.CommodityService;
 import tools.sctrade.companion.domain.commodity.CommoditySubmissionFactory;
 import tools.sctrade.companion.domain.image.ImageManipulation;
 import tools.sctrade.companion.domain.image.ImageWriter;
-import tools.sctrade.companion.domain.image.manipulations.AdjustBrightnessAndContrast;
-import tools.sctrade.companion.domain.image.manipulations.ConvertToGreyscale;
-import tools.sctrade.companion.domain.image.manipulations.InvertColors;
 import tools.sctrade.companion.domain.image.manipulations.UpscaleTo4k;
-import tools.sctrade.companion.domain.ocr.Ocr;
 import tools.sctrade.companion.domain.user.Setting;
 import tools.sctrade.companion.domain.user.SettingRepository;
 import tools.sctrade.companion.domain.user.UserService;
@@ -40,11 +35,12 @@ public class AppConfig {
   public SettingRepository buildSettingRepository() {
     var settingRepository = new SettingRepository();
     settingRepository.set(Setting.MY_IMAGES_PATH,
-        Paths.get("..", "my-images").normalize().toAbsolutePath());
+        Paths.get(".", "my-images").normalize().toAbsolutePath());
     settingRepository.set(Setting.MY_DATA_PATH,
-        Paths.get("..", "my-data").normalize().toAbsolutePath());
+        Paths.get(".", "my-data").normalize().toAbsolutePath());
     settingRepository.set(Setting.OUTPUT_SCREENSHOTS, outputScreenshots);
     settingRepository.set(Setting.OUTPUT_TRANSIENT_IMAGES, outputIntermediaryImages);
+    settingRepository.set(Setting.SC_TRADE_TOOLS_ROOT_URL, "http://localhost:8080");
 
     return settingRepository;
   }
@@ -65,28 +61,9 @@ public class AppConfig {
   }
 
   @Bean("ScTradeToolsClient")
-  public ScTradeToolsClient buildScTradeToolsClient() {
-    return new ScTradeToolsClient();
-  }
-
-  @Bean("CommodityListingsTesseractOcr")
-  public CommodityListingsTesseractOcr buildCommodityTesseractOcr() {
-    List<ImageManipulation> preprocessingManipulations = new ArrayList<>();
-    preprocessingManipulations.add(new ConvertToGreyscale());
-    preprocessingManipulations.add(new InvertColors());
-    preprocessingManipulations.add(new AdjustBrightnessAndContrast(10.0f, 0.0f));
-
-    return new CommodityListingsTesseractOcr(preprocessingManipulations);
-  }
-
-  @Bean("CommodityLocationTesseractOcr")
-  public CommodityLocationTesseractOcr buildCommodityLocationTesseractOcr() {
-    List<ImageManipulation> preprocessingManipulations = new ArrayList<>();
-    preprocessingManipulations.add(new ConvertToGreyscale());
-    preprocessingManipulations.add(new InvertColors());
-    preprocessingManipulations.add(new AdjustBrightnessAndContrast(10.0f, 0.0f));
-
-    return new CommodityLocationTesseractOcr(preprocessingManipulations);
+  public ScTradeToolsClient buildScTradeToolsClient(WebClient.Builder webClientBuilder,
+      SettingRepository settings) {
+    return new ScTradeToolsClient(webClientBuilder, settings);
   }
 
   @Bean("DiskImageWriter")
@@ -96,9 +73,8 @@ public class AppConfig {
 
   @Bean("CommoditySubmissionFactory")
   public CommoditySubmissionFactory buildCommoditySubmissionFactory(UserService userService,
-      @Qualifier("CommodityListingsTesseractOcr") Ocr listingsOcr,
-      @Qualifier("CommodityLocationTesseractOcr") Ocr locationOcr, ImageWriter imageWriter) {
-    return new CommoditySubmissionFactory(userService, listingsOcr, locationOcr, imageWriter);
+      ImageWriter imageWriter) {
+    return new CommoditySubmissionFactory(userService, imageWriter);
   }
 
   @Bean("CommodityService")
