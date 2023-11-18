6abb668be086174dba508721ddaf626f6febf658
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-18 14:52:22 -0500

    Create module file

---
+++ src/main/java/tools/sctrade/companion/module-info.java
@@ -0,0 +1,8 @@
+module sc.trade.companion {
+  requires java.desktop;
+  requires java.logging;
+  requires java.sql;
+
+  requires spring.boot.starter;
+  requires com.formdev.flatlaf.intellijthemes;
+}
