ff2bb6cbf89084b08e86830f678c7a5ba1cd1e11
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-11-19 14:18:07 -0500

    Use gradle plugin to create a standalone version

--- src/main/java/tools/sctrade/companion/module-info.java
+++
@@ -1,8 +0,0 @@
-module sc.trade.companion {
-  requires java.desktop;
-  requires java.logging;
-  requires java.sql;
-
-  requires spring.boot.starter;
-  requires com.formdev.flatlaf.intellijthemes;
-}
