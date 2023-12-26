6b551946d2d7d3baae17f11f31add55675238c29
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2023-12-26 13:07:37 -0500

    Notify users when hte app is outdated

---
+++ src/main/java/tools/sctrade/companion/exceptions/PublicationException.java
@@ -0,0 +1,10 @@
+package tools.sctrade.companion.exceptions;
+
+public class PublicationException extends RuntimeException {
+  private static final long serialVersionUID = 2799328251052287898L;
+
+  public PublicationException(String message) {
+    super(message);
+  }
+
+}
