b31ca5b56da44f698d2e6f571d45922ab5fa2b31
Etienne Lamoureux <3357406+EtienneLamoureux@users.noreply.github.com>
2024-03-04 09:05:53 -0500

    Comment out integration test

--- src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryAccuracyTest.java
+++ src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryAccuracyTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -45,7 +44,7 @@ class CommoditySubmissionFactoryAccuracyTest {
         buildBestEffortCommodityLocationReader(), buildBestEffortCommodityListingFactory());
   }
 
-  @Test
+  // @Test
   void givenCorrectSreenshotThenReadTextAccurately() throws IOException {
     var manipulation = new UpscaleTo4k();
     BufferedImage screenshot =
