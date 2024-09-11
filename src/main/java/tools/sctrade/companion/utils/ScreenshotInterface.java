4c29cb155ebc35a99768504472ff75a748bab683
Jack Greiner <jack@emoss.org>
2024-09-10 21:46:20 -0400

    ScreenshotInterface.java: Whitespace and fix warning

--- src/main/java/tools/sctrade/companion/utils/ScreenshotInterface.java
+++ src/main/java/tools/sctrade/companion/utils/ScreenshotInterface.java
@@ -10,9 +10,9 @@ import org.freedesktop.dbus.types.Variant;
 @DBusInterfaceName(value = "org.freedesktop.portal.Screenshot")
 public interface ScreenshotInterface extends DBusInterface {
 
-    // https://flatpak.github.io/xdg-desktop-portal/portal-docs.html#gdbus-org.freedesktop.portal.Screenshot
-    // Screenshot (IN  s     parent_window,
-    //        IN  a{sv} options,
-    //        OUT o     handle);
-    DBusPath Screenshot(String parentWindow, Map<String, Variant> options);
+  // https://flatpak.github.io/xdg-desktop-portal/portal-docs.html#gdbus-org.freedesktop.portal.Screenshot
+  // Screenshot (IN  s     parent_window,
+  //        IN  a{sv} options,
+  //        OUT o     handle);
+  DBusPath Screenshot(String parentWindow, Map<String, Variant<?>> options);
 }
