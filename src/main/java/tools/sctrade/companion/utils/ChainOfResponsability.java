7bcec2789116bc58cf378e007c067f7b0f4987d6
Etienne Lamoureux <contact@etiennelamoureux.com>
2024-12-02 14:13:22 -0500

    Add generic ChainOfResponsability

---
+++ src/main/java/tools/sctrade/companion/utils/ChainOfResponsability.java
@@ -0,0 +1,28 @@
+package tools.sctrade.companion.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ChainOfResponsability<T extends Object> {
+  private final Logger logger = LoggerFactory.getLogger(ChainOfResponsability.class);
+
+  private ChainOfResponsability<T> next;
+
+  public void setNext(ChainOfResponsability<T> next) {
+    this.next = next;
+  }
+
+  public void process(T value) {
+    if (canHandle(value)) {
+      handle(value);
+    } else if (next != null) {
+      next.process(value);
+    } else {
+      logger.warn("No chain link could handle value '{}'", value);
+    }
+  }
+
+  protected abstract boolean canHandle(T value);
+
+  protected abstract void handle(T value);
+}
