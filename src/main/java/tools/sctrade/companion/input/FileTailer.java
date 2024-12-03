6f0d232c1fe24da543a9259d493147d56ac59ee6
Etienne Lamoureux <contact@etiennelamoureux.com>
2024-12-03 15:23:57 -0500

    Add user-visible logs to the new operation

--- src/main/java/tools/sctrade/companion/input/FileTailer.java
+++ src/main/java/tools/sctrade/companion/input/FileTailer.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tools.sctrade.companion.domain.gamelog.FilePathObserver;
 import tools.sctrade.companion.domain.gamelog.FilePathSubject;
+import tools.sctrade.companion.domain.notification.NotificationService;
+import tools.sctrade.companion.utils.LocalizationUtil;
 
 public class FileTailer extends FilePathObserver {
   private static final Duration DELAY = Duration.ofSeconds(1);
@@ -15,12 +17,17 @@ public class FileTailer extends FilePathObserver {
   private static final Logger logger = LoggerFactory.getLogger(FileTailer.class);
 
   private TailerListener listener;
+  private NotificationService notificationService;
   private Tailer tailer;
 
-  public FileTailer(FilePathSubject subject, TailerListener listener) {
+
+  public FileTailer(FilePathSubject subject, TailerListener listener,
+      NotificationService notificationService) {
     super(subject);
 
+    this.notificationService = notificationService;
     this.listener = listener;
+
     subject.attach(this);
   }
 
@@ -36,8 +43,11 @@ public class FileTailer extends FilePathObserver {
       File file = new File(this.filePath.toString());
       tailer =
           Tailer.builder().setFile(file).setTailerListener(listener).setDelayDuration(DELAY).get();
+      logger.info("Started to tail {}", this.filePath);
+      notificationService.info(LocalizationUtil.get("infoTailingGameLogs"));
     } catch (Exception e) {
       logger.error("Could not tail file '{}'", this.filePath, e);
+      notificationService.error(LocalizationUtil.get("errorTailingGameLogs"));
     }
   }
 
