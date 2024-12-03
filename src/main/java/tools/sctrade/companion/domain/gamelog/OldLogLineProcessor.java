55b77636b82e70924ce191f42b6db8785fe2011e
Etienne Lamoureux <contact@etiennelamoureux.com>
2024-12-03 14:16:58 -0500

    Implement OldLogLineProcessor

---
+++ src/main/java/tools/sctrade/companion/domain/gamelog/OldLogLineProcessor.java
@@ -0,0 +1,46 @@
+package tools.sctrade.companion.domain.gamelog;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tools.sctrade.companion.utils.ChainOfResponsability;
+
+public class OldLogLineProcessor extends ChainOfResponsability<String> {
+  private static final DateTimeFormatter DATE_TIME_FORMATTER =
+      DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ROOT); // 2024-11-13T15:01:11.106Z
+
+  private final Logger logger = LoggerFactory.getLogger(OldLogLineProcessor.class);
+
+  private final Pattern pattern = Pattern.compile("^<(?<timestamp>.{24})> .+");
+  private final Instant applicationOpenTime;
+
+  public OldLogLineProcessor() {
+    this.applicationOpenTime = Instant.now();
+  }
+
+  @Override
+  protected boolean canHandle(String value) {
+    var matcher = pattern.matcher(value);
+
+    if (matcher.matches()) {
+      var timestamp = matcher.group("timestamp");
+      var timestampInstant = LocalDateTime.parse(timestamp, DATE_TIME_FORMATTER)
+          .atZone(ZoneId.systemDefault()).toInstant();
+
+      return timestampInstant.isBefore(applicationOpenTime);
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  protected void handle(String value) {
+    logger.trace("Old log line ignored: '{}'", value);
+  }
+
+}
---
+++ src/test/java/tools/sctrade/companion/domain/gamelog/OldLogLineProcessorTest.java
@@ -0,0 +1,39 @@
+package tools.sctrade.companion.domain.gamelog;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OldLogLineProcessorTest {
+  private OldLogLineProcessor oldLogLineProcessor;
+
+  @BeforeEach
+  void setUp() {
+    oldLogLineProcessor = new OldLogLineProcessor();
+  }
+
+  @Test
+  void givenOldLogLineWhenCheckingIfCanHandleThenReturnTrue() {
+    var canHandle = oldLogLineProcessor
+        .canHandle("<2024-11-13T15:01:11.102Z> Requesting game mode Frontend_Main/SC_Frontend");
+
+    assertTrue(canHandle);
+  }
+
+  @Test
+  void givenNewLogLineWhenCheckingIfCanHandleThenReturnFalse() {
+    var canHandle = oldLogLineProcessor.canHandle(
+        "<2124-11-13T15:01:10.344Z> [Notice] <BeginAsyncReset> CEntitySystem::BeginAsyncReset called! Set m_bResetting = true [Team_EntitySystemTech][Entity][EntitySystem]");
+
+    assertFalse(canHandle);
+  }
+
+  @Test
+  void givenInvalidLogLineWhenCheckingIfCanHandleThenReturnFalse() {
+    var canHandle = oldLogLineProcessor.canHandle("bonjour");
+
+    assertFalse(canHandle);
+  }
+}
