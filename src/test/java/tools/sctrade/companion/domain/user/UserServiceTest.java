4b30d4a84378a16ca8152a3dbfc17209a4beb2ab
Etienne Lamoureux <contact@etiennelamoureux.com>
2025-01-19 16:38:45 -0500

    Add and apply spotless

--- src/test/java/tools/sctrade/companion/domain/user/UserServiceTest.java
+++ src/test/java/tools/sctrade/companion/domain/user/UserServiceTest.java
@@ -1,6 +1,7 @@
 package tools.sctrade.companion.domain.user;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
--- src/test/java/tools/sctrade/companion/utils/MathUtilTest.java
+++ src/test/java/tools/sctrade/companion/utils/MathUtilTest.java
@@ -2,6 +2,7 @@ package tools.sctrade.companion.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
