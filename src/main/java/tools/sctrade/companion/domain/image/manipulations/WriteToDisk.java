00025d76b2b65aa597cbc0cfcd3a88cd3f65f901
Etienne Lamoureux <contact@etiennelamoureux.com>
2025-12-30 15:40:51 -0500

    Type image writer

--- src/main/java/tools/sctrade/companion/domain/image/manipulations/WriteToDisk.java
+++ src/main/java/tools/sctrade/companion/domain/image/manipulations/WriteToDisk.java
@@ -1,6 +1,8 @@
 package tools.sctrade.companion.domain.image.manipulations;
 
 import java.awt.image.BufferedImage;
+import java.nio.file.Path;
+import java.util.Optional;
 import tools.sctrade.companion.domain.image.ImageManipulation;
 import tools.sctrade.companion.domain.image.ImageType;
 import tools.sctrade.companion.domain.image.ImageWriter;
@@ -9,9 +11,9 @@ import tools.sctrade.companion.domain.image.ImageWriter;
  * Writes an image to disk. Mostly used to debug by inserting between pre-processing steps.
  */
 public class WriteToDisk implements ImageManipulation {
-  private ImageWriter imageWriter;
+  private ImageWriter<Optional<Path>> imageWriter;
 
-  public WriteToDisk(ImageWriter imageWriter) {
+  public WriteToDisk(ImageWriter<Optional<Path>> imageWriter) {
     this.imageWriter = imageWriter;
   }
 
