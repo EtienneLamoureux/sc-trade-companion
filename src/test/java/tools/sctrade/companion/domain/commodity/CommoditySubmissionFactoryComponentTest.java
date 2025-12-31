592e44224545740f6ff82b90f001441dc20c8b5f
Etienne Lamoureux <contact@etiennelamoureux.com>
2025-12-31 11:42:09 -0500

    Add more test cases

--- src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryComponentTest.java
+++ src/test/java/tools/sctrade/companion/domain/commodity/CommoditySubmissionFactoryComponentTest.java
@@ -55,7 +55,7 @@ class CommoditySubmissionFactoryComponentTest {
 
   @Test
   void bonjour() throws IOException, URISyntaxException {
-    var filename = "arc-l3-buy-1";
+    var filename = "arc-l1-sell-1";
 
     String resourcePath = "/kiosks/commodity/images/" + filename + ".jpg";
     when(diskImageWriter.write(any(), any()))
