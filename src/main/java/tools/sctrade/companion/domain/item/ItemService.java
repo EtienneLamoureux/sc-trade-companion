a2566598c53b91a8bd2c5412f1436bbbeca639a1
Etienne Lamoureux <contact@etiennelamoureux.com>
2026-03-23 15:47:23 -0400

    Implement ItemService

--- src/main/java/tools/sctrade/companion/domain/item/ItemService.java
+++ src/main/java/tools/sctrade/companion/domain/item/ItemService.java
@@ -1,17 +1,27 @@
 package tools.sctrade.companion.domain.item;
 
 import java.awt.image.BufferedImage;
+import java.util.Collection;
 import tools.sctrade.companion.domain.notification.NotificationService;
 import tools.sctrade.companion.utils.AsynchronousProcessor;
 
 public class ItemService extends AsynchronousProcessor<BufferedImage> {
 
-  protected ItemService(NotificationService notificationService) {
+  private ItemSubmissionFactory submissionFactory;
+  private Collection<AsynchronousProcessor<ItemSubmission>> publishers;
+
+  public ItemService(NotificationService notificationService,
+      ItemSubmissionFactory submissionFactory,
+      Collection<AsynchronousProcessor<ItemSubmission>> publishers) {
     super(notificationService);
+
+    this.submissionFactory = submissionFactory;
+    this.publishers = publishers;
   }
 
   @Override
   protected void process(BufferedImage screenCapture) throws Exception {
-
+    ItemSubmission submission = submissionFactory.build(screenCapture);
+    publishers.forEach(p -> p.processAsynchronously(submission));
   }
 }
